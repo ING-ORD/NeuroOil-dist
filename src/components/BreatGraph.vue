5c0bfa0bab26332e05552484ce90b907c39dcc39
ING-ORD <igant.markov345@gmail.com>
2020-09-19 23:33:29 +0500

    add ChartJS

---
+++ src/components/BreatGraph.vue
@@ -0,0 +1,17 @@
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+    name: 'BreatGraph',
+    extends: Line,
+    props: ['chartdata', 'options'],
+    mounted () {
+            this.renderChart(this.chartdata, this.options)
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,128 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    HEADER
+    <div class="small">
+      <line-chart :chart-data="datacollection" :options="chartOptions"></line-chart>
+      <button @click="fillData()">Randomize</button>
+    </div>
   </div>
 </template>
 
 <script>
+import LineChart from '@/components/LineChart.js'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+      LineChart
+    },
+    data () {
+      return {
+        chartOptions: null,
+        datacollection: null,
+        labels: [],
+        datasetsData: []
+      }
+    },
+    mounted () {
+      this.fillData()
+      setInterval(() => {
+        this.moveData()
+      },2000)
+    },
+    methods: {
+      fillData () {
+        
+        this.chartOptions = {
+          responsive: true,
+          scales: {
+            yAxes: [{
+              ticks: {
+                // the data minimum used for determining the ticks is Math.min(dataMin, suggestedMin)
+                suggestedMin: 10,
+
+                // the data maximum used for determining the ticks is Math.max(dataMax, suggestedMax)
+                suggestedMax: 50
+              }
+            }]
+          },
+          animation: {
+              duration: 0
+            
+          }
+        //   legend: {
+        //     display: true,
+        //     labels: {
+        //         fontColor: 'rgb(255, 99, 132)'
+        //     }
+        // }
+        }
+
+        this.datacollection = {
+          labels: this.fillArray(),
+          datasets: [
+            {
+              label: 'Brent',
+              backgroundColor: 'green',
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              data: this.fillArray(),
+              pointRadius: 1,
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (30 - 5 + 1)) + 11
+      },
+      fillArray(){
+        let data = []
+        for (let i = 0; i<=25; i++){
+          data.push(this.getRandomInt())
+        }
+        return data
+      },
+      moveData(){
+        const dataPoint = this.getRandomInt()
+
+        const len = this.datacollection.datasets[0].data.length
+        for (let i=0; i<len-1; i++){
+          this.datacollection.datasets[0].data[i] = this.datacollection.datasets[0].data[i+1]
+        }
+        this.datacollection.datasets[0].data.push(dataPoint) 
+        // const lablePoint = this.getRandomInt()
+
+        // this.datacollection.labels.push(lablePoint)
+        // this.datacollection.datasets[0].data.push(dataPoint)
+
+        // this.datacollection.labels.shift()
+        // this.datacollection.datasets[0].data.shift()
+
+        this.datacollection = {
+          labels: this.datacollection.labels,
+          datasets: [
+            {
+              label: 'Brent',
+              backgroundColor: 'green',
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              data: this.datacollection.datasets[0].data,
+              pointRadius: 1,
+            }
+          ]
+        }
+
+      }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
 h3 {
   margin: 40px 0 0;
 }
