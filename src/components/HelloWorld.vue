2bbdf34d5b0da0a4205364d776ca5b61d5c172a8
ING-ORD <igant.markov345@gmail.com>
2020-09-20 22:15:58 +0500

    add vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,140 +1,151 @@
 <template>
-  <div class="hello">
-    HEADER
-    <div class="small">
-      <line-chart :chart-data="datacollection" :options="chartOptions"></line-chart>
-      <button @click="fillData()">Randomize</button>
-    </div>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import LineChart from '@/components/LineChart.js'
+  export default {
+    name: 'HelloWorld',
 
-export default {
-  name: 'HelloWorld',
-  components: {
-      LineChart
-    },
-    data () {
-      return {
-        chartOptions: null,
-        datacollection: null,
-        labels: [],
-        datasetsData: []
-      }
-    },
-    mounted () {
-      this.fillData()
-      setInterval(() => {
-        this.moveData()
-      },2000)
-    },
-    methods: {
-      fillData () {
-        
-        this.chartOptions = {
-          responsive: true,
-          scales: {
-            yAxes: [{
-              ticks: {
-                // the data minimum used for determining the ticks is Math.min(dataMin, suggestedMin)
-                suggestedMin: 10,
-
-                // the data maximum used for determining the ticks is Math.max(dataMax, suggestedMax)
-                suggestedMax: 50
-              }
-            }]
-          },
-          animation: {
-              duration: 0
-            
-          }
-        //   legend: {
-        //     display: true,
-        //     labels: {
-        //         fontColor: 'rgb(255, 99, 132)'
-        //     }
-        // }
-        }
-
-        this.datacollection = {
-          labels: this.fillArray(),
-          datasets: [
-            {
-              label: 'Brent',
-              backgroundColor: 'green',
-              fill: false,
-              borderColor: 'rgb(75, 192, 192)',
-              data: this.fillArray(),
-              pointRadius: 1,
-            }
-          ]
-        }
-      },
-      getRandomInt () {
-        return Math.floor(Math.random() * (30 - 5 + 1)) + 11
-      },
-      fillArray(){
-        let data = []
-        for (let i = 0; i<=25; i++){
-          data.push(this.getRandomInt())
-        }
-        return data
-      },
-      moveData(){
-        const dataPoint = this.getRandomInt()
-
-        const len = this.datacollection.datasets[0].data.length
-        for (let i=0; i<len-1; i++){
-          this.datacollection.datasets[0].data[i] = this.datacollection.datasets[0].data[i+1]
-        }
-        this.datacollection.datasets[0].data.push(dataPoint) 
-        // const lablePoint = this.getRandomInt()
-
-        // this.datacollection.labels.push(lablePoint)
-        // this.datacollection.datasets[0].data.push(dataPoint)
-
-        // this.datacollection.labels.shift()
-        // this.datacollection.datasets[0].data.shift()
-
-        this.datacollection = {
-          labels: this.datacollection.labels,
-          datasets: [
-            {
-              label: 'Brent',
-              backgroundColor: 'green',
-              fill: false,
-              borderColor: 'rgb(75, 192, 192)',
-              data: this.datacollection.datasets[0].data,
-              pointRadius: 1,
-            }
-          ]
-        }
-
-      }
-    }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.small {
-    max-width: 600px;
-    margin:  150px auto;
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
   }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- <BreatGraph/> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <BreatGraph/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-// import BreatGraph from '@/components/BreatGraph.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import BreatGraph from '@/components/BreatGraph.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-    // BreatGraph
+    // HelloWorld,
+    BreatGraph
   }
 }
 </script>
